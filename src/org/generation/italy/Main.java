6d29e412b77442f094671674952ff783dc940f24
Vincenzo Colluto <enzocoll@tiscali.it>
2024-05-20 11:03:23 +0200

    gestione più schede

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -36,23 +36,31 @@ Per i più "temerari": dare la possibilità all'utente di avere più di una sche
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> schedaUtente = new ArrayList<Integer>();	//le prime 5 posizioni sono la prima riga, le successive 5 la seconda, ecc.
+		ArrayList<ArrayList<Integer>> schedeUtente=new ArrayList<ArrayList<Integer>>();	//le prime 5 posizioni sono la prima riga, le successive 5 la seconda, ecc.
 		HashSet<Integer> numeriEstratti=new HashSet<Integer>();
-		
+		ArrayList<Integer> schedaUtente;
 		
 		Random r=new Random();
 		Scanner sc=new Scanner(System.in);
-		int n, estrattiRiga, estrattiScheda;
+		int n,i,j, estrattiRiga, estrattiScheda, nrSchede;
 		boolean cinquina=false, bingo=false;
 		boolean attesaBingo=false;		//indica se sono in attesa del bingo 
 		
+		System.out.println("Benvenuto nel gioco del Bingo!");
+		System.out.print("Quante schede vuoi acquistare? ");
+		nrSchede=sc.nextInt();
+		sc.nextLine();
 		
-		do {
-			n=r.nextInt(90)+1;					//casuale da 1 a 90
-			if (!schedaUtente.contains(n))
-				schedaUtente.add(n);			//se il numero non è contenuto nella scheda lo aggiungo
-		} while (schedaUtente.size()<15);		//torno indietro se nella scheda ci sono meno di 15 numeri
-
+		//inizializzo l'array delle schede
+		for (i=1;i<=nrSchede;i++) {		//per ogni scheda
+			schedaUtente=new ArrayList<Integer>();
+			schedeUtente.add(schedaUtente);
+			do {
+				n=r.nextInt(90)+1;					//casuale da 1 a 90
+				if (!schedaUtente.contains(n))
+					schedaUtente.add(n);			//se il numero non è contenuto nella scheda lo aggiungo
+			} while (schedaUtente.size()<15);		//torno indietro se nella scheda ci sono meno di 15 numeri
+		}
 		
 		
 		
@@ -64,33 +72,37 @@ public class Main {
 				
 				System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 				System.out.println("Numero estratto: "+n);
-				
-				estrattiRiga=0;
-				estrattiScheda=0;
-				//stampo la scheda su tre righe
-				for (int i=0;i<schedaUtente.size();i++) {
-					if (numeriEstratti.contains(schedaUtente.get(i))) {	//se il numero della scheda è stato estratto lo evidenzio
-						System.out.print("\u001b[1;34;34m");		//colore blu
-						System.out.print("\u001b[1m");		//grassetto
-						estrattiRiga++;
-						estrattiScheda++;
-						if (estrattiRiga==5)
-							cinquina=true;
-						if (estrattiScheda==15) {
-							bingo=true;
+				for (j=0;j<nrSchede;j++) {		//per ogni scheda
+					schedaUtente=schedeUtente.get(j);
+					
+					estrattiRiga=0;
+					estrattiScheda=0;
+					//stampo la scheda su tre righe
+					for (i=0;i<schedaUtente.size();i++) {
+						if (numeriEstratti.contains(schedaUtente.get(i))) {	//se il numero della scheda è stato estratto lo evidenzio
+							System.out.print("\u001b[1;34;34m");		//colore blu
+							System.out.print("\u001b[1m");		//grassetto
+							estrattiRiga++;
+							estrattiScheda++;
+							if (estrattiRiga==5)
+								cinquina=true;
+							if (estrattiScheda==15) {
+								bingo=true;
+							}
+								
+						}							
+						else
+							System.out.print("\u001b[0m");		//testo normale						
+						System.out.print(schedaUtente.get(i)+" ");
+						if (i==4||i==9)	{				//cambio riga => azzero il contatore della cinquina
+							System.out.println();
+							estrattiRiga=0;
 						}
-							
-					}							
-					else
-						System.out.print("\u001b[0m");		//testo normale						
-					System.out.print(schedaUtente.get(i)+" ");
-					if (i==4||i==9)	{				//cambio riga => azzero il contatore della cinquina
-						System.out.println();
-						estrattiRiga=0;
-					}
-				}	
-				System.out.print("\u001b[0m");		//testo normale		
-				System.out.println();
+					}	
+					System.out.print("\u001b[0m");		//testo normale		
+					System.out.println("\n");
+				}
+				
 				if (cinquina && !attesaBingo) {		// se ho fatto la cinquina e non sono in attesa del bingo mostro il messaggio "Cinquina"
 					System.out.println("Cinquina!");
 					attesaBingo=true;		//non mi interessa più la cinquina
@@ -103,6 +115,7 @@ public class Main {
 			}
 			
 		} while (numeriEstratti.size()<90 && !bingo);		//torno indietro se nella scheda ci sono meno di 15 numeri e non ho fatto bingo
+		sc.close();
 	}
 
 }
